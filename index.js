8dcffaa922c2ab972acadedc2e9fd2531e2dddb5
Fidel Castro <fidelcastro4080@gmail.com>
2025-11-14 16:34:42 +0300

    Fixed a bug making the game not to run

--- index.js
+++ index.js
@@ -10,19 +10,18 @@ let running = true;
 
 
 button.onclick = function(){
-  while(running){
      
     guess = document.getElementById("guess").value;
     guess = Number(guess);
 
     if(isNaN(guess)){
       output.textContent = "Please enter a valid number";
-      output.style.colour = "red";
+      output.style.color = "red";
     }
 
     else if(guess < minNum || guess > maxNum){
       output.textContent = (`Only numbers between ${minNum} and ${maxNum} are allowed!`);
-      output.style.colour = "red";
+      output.style.color = "red";
     }
 
     else{
@@ -30,18 +29,18 @@ button.onclick = function(){
 
       if(guess < answer){
          output.textContent = ("TOO LOW! PLEASE TRY ANOTHER GUESS!");
-         output.style.colour = "orange";
+         output.style.color = "orange";
       }
 
       else if(guess > answer){
          output.textContent = ("TOO HIGH! PLEASE TRY ANOTHER GUESS!");
-         output.style.colour = "yellow";
+         output.style.color = "yellow";
       }
 
       else{
          output.textContent = (`CORRECT!! The answer was ${answer}. You took ${attempts} attempts`);
-         output.style.colour = "green";
+         output.style.color = "green";
+         running = false;
       }
     }
   }
-}
